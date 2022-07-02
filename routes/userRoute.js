e248668a6ae45a5d018b7d3bb5e2eeb74c92f00a
Mahmoud Samir <menatosamir55@gmail.com>
2022-07-02 03:20:10 +0200

    adding user routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,4 +1,12 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const {
+  createUser,
+  getUser,
+  getUsers,
+} = require("../controllers/userController");
 
+router.route('/').post(createUser).get(getUsers)
 
-module.exports = router
+router.route('/:id').get(getUser)
+
+module.exports = router;
