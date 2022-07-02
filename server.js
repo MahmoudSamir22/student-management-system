7a1892d71cbc71ff55caac2f7e866e230688e2b1
Mahmoud Samir <menatosamir55@gmail.com>
2022-07-02 03:17:28 +0200

    remove .env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config({path: 'config.env'})
 
 
 const dbConnection = require('./database/dbConnection')
